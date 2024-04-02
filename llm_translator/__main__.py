f9eff60169eac0c8291ca70606cda305bbaf8c2b
shin-hama <htysivista@gmail.com>
2024-04-02 09:09:37 +0900

    MAINT: change package name

---
+++ llm_translator/__main__.py
@@ -0,0 +1,20 @@
+import fire
+from logging import getLogger, StreamHandler, DEBUG
+
+from .application.translate_html import exec
+
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+
+def main(root: str):
+    exec(root, logger)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
