99b9e027b838fe6a885455a0d9b863e8737dd5b1
shin-hama <htysivista@gmail.com>
2024-04-02 09:13:46 +0900

    FIX: 初回読み込みに時間がかかる問題を修正

--- llm_translator/domain/translator/__init__.py
+++ llm_translator/domain/translator/__init__.py
@@ -1,10 +1,13 @@
 from logging import Logger
 from typing import Optional
-from .models.mbart import MbartModel
 
 
 class Translator:
+
     def __init__(self, logger: Optional[Logger]):
+        # model ファイルの読み込みに時間がかかるのでここで初期化する
+        from .models.mbart import MbartModel
+
         if logger is not None:
             logger.info("Initialize Translator...")
         self.model = MbartModel()
